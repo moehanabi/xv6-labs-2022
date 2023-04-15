2c3340de03bea62aac5bfeb0d20a2767adc2227d
Brilliant Hanabi <moehanabichan@outlook.com>
2023-04-15 14:08:45 +0800

    add count_proc()

--- kernel/proc.c
+++ kernel/proc.c
@@ -684,3 +684,18 @@ procdump(void)
     printf("\n");
   }
 }
+
+uint64 count_proc(void)
+{
+  struct proc *p;
+  uint64 count = 0;
+
+  for(p = proc; p < &proc[NPROC]; p++) {
+    acquire(&p->lock);
+    if(p->state != UNUSED) {
+      count++;
+    }
+    release(&p->lock);
+  }
+  return count;
+}
